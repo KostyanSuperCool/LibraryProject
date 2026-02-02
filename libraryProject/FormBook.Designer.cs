3c551ee9ba8663b60c5ad808d8f641bb44296656
Kiselev Kostya <kiselev.k0101@gmail.com>
2026-02-02 11:25:58 +0500

    Создание формы FormBook

---
+++ libraryProject/FormBook.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace libraryProject
+{
+    partial class FormBook
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            DataGridViewCellStyle dataGridViewCellStyle2 = new DataGridViewCellStyle();
+            panelTop = new Panel();
+            dgvBook = new DataGridView();
+            btnLogin = new Button();
+            lblUserName = new Label();
+            panelTop.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dgvBook).BeginInit();
+            SuspendLayout();
+            // 
+            // panelTop
+            // 
+            panelTop.Controls.Add(lblUserName);
+            panelTop.Controls.Add(btnLogin);
+            panelTop.Dock = DockStyle.Top;
+            panelTop.Location = new Point(10, 10);
+            panelTop.Name = "panelTop";
+            panelTop.Padding = new Padding(0, 0, 0, 10);
+            panelTop.Size = new Size(964, 40);
+            panelTop.TabIndex = 0;
+            // 
+            // dgvBook
+            // 
+            dgvBook.AllowUserToAddRows = false;
+            dgvBook.AllowUserToDeleteRows = false;
+            dgvBook.AutoSizeRowsMode = DataGridViewAutoSizeRowsMode.AllCells;
+            dgvBook.BackgroundColor = Color.White;
+            dgvBook.BorderStyle = BorderStyle.None;
+            dgvBook.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewCellStyle2.Alignment = DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = SystemColors.Window;
+            dataGridViewCellStyle2.Font = new Font("Times New Roman", 12F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            dataGridViewCellStyle2.ForeColor = SystemColors.ControlText;
+            dataGridViewCellStyle2.SelectionBackColor = SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = DataGridViewTriState.True;
+            dgvBook.DefaultCellStyle = dataGridViewCellStyle2;
+            dgvBook.Dock = DockStyle.Fill;
+            dgvBook.Location = new Point(10, 50);
+            dgvBook.MultiSelect = false;
+            dgvBook.Name = "dgvBook";
+            dgvBook.ReadOnly = true;
+            dgvBook.RowHeadersVisible = false;
+            dgvBook.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            dgvBook.Size = new Size(964, 601);
+            dgvBook.TabIndex = 1;
+            // 
+            // btnLogin
+            // 
+            btnLogin.BackColor = Color.FromArgb(74, 111, 165);
+            btnLogin.Dock = DockStyle.Right;
+            btnLogin.FlatAppearance.BorderSize = 0;
+            btnLogin.FlatStyle = FlatStyle.Flat;
+            btnLogin.Location = new Point(814, 0);
+            btnLogin.Name = "btnLogin";
+            btnLogin.Size = new Size(150, 30);
+            btnLogin.TabIndex = 3;
+            btnLogin.Text = "Войти";
+            btnLogin.UseVisualStyleBackColor = false;
+            // 
+            // lblUserName
+            // 
+            lblUserName.AutoSize = true;
+            lblUserName.Dock = DockStyle.Right;
+            lblUserName.Location = new Point(769, 0);
+            lblUserName.Name = "lblUserName";
+            lblUserName.Size = new Size(45, 19);
+            lblUserName.TabIndex = 4;
+            lblUserName.Text = "label1";
+            lblUserName.TextAlign = ContentAlignment.MiddleRight;
+            // 
+            // FormBook
+            // 
+            AutoScaleDimensions = new SizeF(9F, 19F);
+            AutoScaleMode = AutoScaleMode.Font;
+            BackColor = Color.White;
+            ClientSize = new Size(984, 661);
+            Controls.Add(dgvBook);
+            Controls.Add(panelTop);
+            Font = new Font("Times New Roman", 12F, FontStyle.Regular, GraphicsUnit.Point, 204);
+            Margin = new Padding(4, 4, 4, 4);
+            Name = "FormBook";
+            Padding = new Padding(10);
+            StartPosition = FormStartPosition.CenterScreen;
+            Text = "Список книг";
+            panelTop.ResumeLayout(false);
+            panelTop.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)dgvBook).EndInit();
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private Panel panelTop;
+        private DataGridView dgvBook;
+        private Label lblUserName;
+        private Button btnLogin;
+    }
+}
---
+++ libraryProject/FormBook.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace libraryProject
+{
+    public partial class FormBook : Form
+    {
+        public FormBook()
+        {
+            InitializeComponent();
+        }
+    }
+}
